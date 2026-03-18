a93db75703a2ecfd2bf31703dac6bd65296821af
Noah Kirwan <mrnoahkirwan@gmail.com>
2026-03-18 16:55:42 +0000

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
 console.log("Hello from the Web App Dev lab!");
+
+$('.ui.rating')
+  .rating()
+;
+
+let instructor = "Elie";
+
+if (instructor === "Elie") {
+    console.log("Yes!");
+} else {
+    console.log("No");
+}
+
